e9fbad3230e8a26660425e6ba7a67d6a61a362a5
Vitaly2019 <vegasssssssssss@gmail.com>
2019-01-04 22:26:06 +0300

    header fix 1

--- ds_app/static/ds_app/assets/js/main.js
+++ ds_app/static/ds_app/assets/js/main.js
@@ -2,6 +2,7 @@ $(document).on('ready', function(){
 
 content=document.getElementById("inlineFormCustomSelectPref").value;
 
+/*
 if(content == 'Петрозаводск'){
 	document.getElementById("adr-info").innerHTML='Лесной проспект д5'
 	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> 8-8142-77-77-77'
@@ -14,13 +15,63 @@ else {
 	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> 8-0495-1234-55-11'
 	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
 }
+*/
+
+if(content == 'Петрозаводск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 125'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 09:00 до 17:00'
+}
+else if(content =='Москва'){
+
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 126'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+else if(content =='Екатеринбург'){
+
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 127'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)'
+}
+else if(content =='Новосибирск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 128'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +4ч. (МСК)'
+}
+else if(content =='Челябинск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 129'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)'
+}
+else if(content =='Хабаровск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 130'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +7ч. (МСК)'
+}
+else if(content =='Казань'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 131'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+else if(content =='Нижний Новгород'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 132'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+else if(content =='Уфа'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 133'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)'
+}
+else if(content =='Ростов-на-Дону'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 134'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+else if(content =='Краснодар'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 135'
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+
+
 
 });
 
 $(document).on('click', '#inlineFormCustomSelectPref', function(){
 
 content=document.getElementById("inlineFormCustomSelectPref").value;
-
+/*
 if(content == 'Петрозаводск'){
 	document.getElementById("adr-info").innerHTML='Лесной проспект д5'
 	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> 8-8142-77-77-77'
@@ -32,6 +83,53 @@ else {
 	document.getElementById("adr-info").innerHTML='Кательническая набережная 20'
 	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> 8-0495-1234-55-11'
 	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00'
+}
+*/
+
+if(content == 'Петрозаводск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 125';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 09:00 до 17:00';
+}
+else if(content =='Москва'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 126';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00';
+}
+else if(content =='Екатеринбург'){
+
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 127';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)';
+}
+else if(content =='Новосибирск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 128';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +4ч. (МСК)';
+}
+else if(content =='Челябинск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 129';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)';
+}
+else if(content =='Хабаровск'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 130';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +7ч. (МСК)';
+}
+else if(content =='Казань'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 131';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00';
+}
+else if(content =='Нижний Новгород'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 132';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00';
+}
+else if(content =='Уфа'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 133';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00 +2ч. (МСК)';
+}
+else if(content =='Ростов-на-Дону'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 134';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00';
+}
+else if(content =='Краснодар'){
+	document.getElementById("adr-tel").innerHTML='<i class="fas fa-phone" style="color:#FDC300"></i> +7-(495)-887-88-88 доб. 135';
+	document.getElementById("adr-grafic").innerHTML='График работы: с 10:00 до 17:00';
 }
 
 });
